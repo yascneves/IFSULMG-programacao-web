d49049adeaff191822dc7d22f87f578f2f99b0ec
Yasmim Neves <yascneves@hotmail.com>
2023-12-11 22:24:52 -0300

    variáveis js

---
+++ aulas-js/4_variaveis/js/script.js
@@ -0,0 +1,21 @@
+// nesse arquivo, aprendemos diferentes jeitos de declarar variáveis no javascript.
+var numerocompra = 1
+
+var nomecliente = 'João'
+
+console.log(nomecliente)
+
+console.log(numerocompra)
+
+numerocompra = 'Matheus'
+
+console.log(nomecliente)
+
+var $nomecliente = 'abc'
+console.log($nomecliente)
+
+var _nomecliente = 'abc2'
+console.log(_nomecliente)
+
+var meuPrimeiroNome = 'Yasmim'
+console.log(meuPrimeiroNome)
