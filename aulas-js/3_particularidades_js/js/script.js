72f2f3aef11ce6be31d5fb0e80afbd70b02b4c35
Yasmim Neves <yascneves@hotmail.com>
2023-12-11 22:09:10 -0300

    introdução ao javascript

---
+++ aulas-js/3_particularidades_js/js/script.js
@@ -0,0 +1,12 @@
+console.log('Primeiro')
+console.log('Último')
+console.log('Mais um console.log')
+
+// declara variável
+var nomesugestivo = 1
+let segundonome = 2
+const terceironome = 3
+
+nomesugestivo = 'textinho de exemplo para mostrar no navegador'
+
+console.log(nomesugestivo)
