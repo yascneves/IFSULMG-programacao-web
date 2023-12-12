2d0828268dfd906d2baeefae7a07390a73952d1c
Yasmim Neves <yascneves@hotmail.com>
2023-12-11 23:14:37 -0300

    aula javascript

---
+++ aulas-js/5_numbers/js/script.js
@@ -0,0 +1,22 @@
+// nesse arquivo, aprendemos diferentes jeitos de declarar variáveis no javascript.
+var numero = 5;
+
+console.log(numero)
+
+console.log(typeof(numero));
+
+var float = 5.32;
+console.log(float);
+
+var textoNumero = "532";
+
+console.log(textoNumero);
+console.log(typeof(textoNumero));
+
+console.log(textoNumero);
+console.log(typeof(textoNumero));
+
+console.log(textoNumero);
+console.log(textoNumero);
+
+
